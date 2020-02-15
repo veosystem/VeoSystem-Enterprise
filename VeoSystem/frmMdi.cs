b2353ce7dd899bdc9674595a92dfc4332b42d29c
Luis Alberto Velarde Merino <veo_system@hotmail.com>
2020-02-15 17:21:28 -0600

    Inicio

--- VeoSystem/frmMdi.cs
+++ VeoSystem/frmMdi.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace VeoSystem
 {
-    public partial class Form1 : Form
+    public partial class frmMdi : Form
     {
-        public Form1()
+        public frmMdi()
         {
             InitializeComponent();
         }
