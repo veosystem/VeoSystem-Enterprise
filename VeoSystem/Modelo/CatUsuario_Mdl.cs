d93a7556801e3d39e7d9d02122237b74df9b291e
luisvelarde80 <54183852+luisvelarde80@users.noreply.github.com>
2020-02-19 17:05:03 -0600

    Nuevo 2

---
+++ VeoSystem/Modelo/CatUsuario_Mdl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeoSystem.Modelo
+{
+    public class CatUsuario_Mdl
+    {
+
+        public int idUsuario { get; set; }
+
+        public string Usuario { get; set; }
+
+        public string Password { get; set; }
+
+        public int idRol { get; set; }
+
+        public int Estatus { get; set; }
+
+        public int CambioPass { get; set; }
+
+    }
+
+}
